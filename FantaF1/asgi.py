f3a2619db79ffd82acbb60c2914c8957bf269372
TheGabro <gabrosciurti@gmail.com>
2025-06-28 21:18:15 +0200

    Rename App and fix migration error

--- FantaF1/asgi.py
+++ FantaF1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fanta_f1 project.
+ASGI config for FantaF1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fanta_f1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FantaF1.settings')
 
 application = get_asgi_application()
--- FantaF1/urls.py
+++ FantaF1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for fanta_f1 project.
+URL configuration for FantaF1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
