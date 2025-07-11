9b5f09ad112cc412fbe8c5ed092709a6db8c20ec
GSciurti <gsciurti@euronext.com>
2025-07-11 15:41:44 +0200

    get season initial info with jolpica API

--- fantaApp/services/jolpicaSource.py
+++ fantaApp/services/jolpicaSource.py
@@ -3,12 +3,11 @@ import requests
 BASE_URL = 'https://api.jolpi.ca/ergast/f1/'
 
 
-def get_drivers(season:int):
+def get_drivers(season:int) -> list[dict]:
     drivers_url = f"{BASE_URL}/{season}/drivers.json"
     driver_r = requests.get(drivers_url, timeout=10)
     driver_r.raise_for_status()
     drivers = []
-    constructor = []
     for d in driver_r.json()["MRData"]["DriverTable"]["Drivers"]:
         driver_id = d["driverId"]
         constructor_url = f"{BASE_URL}/{season}/drivers/{driver_id}/constructors"
@@ -24,12 +23,35 @@ def get_drivers(season:int):
             "short_name": d["code"],
             "team": constructor[["constructorId"]]
         })
+    return drivers 
 
-        constructor.append({
-            "name": constructor['name'],
-            "nationality":constructor['nationality'],
-            "api_id": constructor['constructorId'],
-            "short_name": constructor['name'][:3].upper()
+def get_teams(season:int) -> list[dict]:
+    teams_url = f"{BASE_URL}/{season}/constructors"
+    teams_r = requests.get(teams_url, timeout = 10)
+    teams_r.raise_for_status()
+    teams = []
+    for c in teams_r.json()["MRData"]["ConstructorTable"]["Constructors"]:
+        teams.append({
+            "name": c['name'],
+            "nationality":c['nationality'],
+            "api_id": c['constructorId'],
+            "short_name": c['name'][:3].upper()
             }
         )
-    return drivers
+
+    return teams
+
+def get_circuits(season: int) -> list[dict]:
+    circuits_url = f'{BASE_URL}/{season}/circuits'
+    circuits_r = requests.get(circuits_url, timeout = 10)
+    circuits_r.raise_for_status()
+    circuits = []
+    for c in circuits_r.json()["MRData"]["CircuitTable"]["Circuits"]:
+        circuits.append({
+            "name": c["circuitName"],
+            "country": c["Location"]["country"],
+            "location": c["Location"]["locality"],
+            "api_id": c["circuitId"]
+        })
+
+    
