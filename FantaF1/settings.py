5fa4fb18bb1355b5c43c9331b71ce6e9f77ffce0
GSciurti <gsciurti@euronext.com>
2026-07-31 15:31:23 +0200

    chore: made compatible with mac os

--- FantaF1/settings.py
+++ FantaF1/settings.py
@@ -73,13 +73,19 @@ WSGI_APPLICATION = 'FantaF1.wsgi.application'
 # Grazie alla variabile di ambiente il default database sarà sqlite3, 
 # ma in produzione useremo postgresql
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-    )
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
